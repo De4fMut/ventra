a7aa641d60c6782221373c9b47cca839fcc27b9d
De4fMut <vendetta-7@bk.ru>
2022-12-15 15:49:01 +0300

    готово

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat et unde iure ab fugiat quisquam, quos optio nostrum, quia itaque quasi molestiae voluptatibus sunt omnis praesentium veniam sapiente placeat fugit.</p>
+
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
